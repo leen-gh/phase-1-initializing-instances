af20a815c8fc5daaea793dfe73e181b07c10b2f8
leen-gh <leen.ghaleb.d@gmail.com>
2023-12-20 01:17:59 +0300

    solved Assigment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here
+
+class Breakfast {
+    constructor(food, drink) {
+      this.drink = drink;
+      this.food = food;
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink) {
+      this.drink = drink;
+      this.soup = soup;
+      this.salad = salad
+
+    }
+}
+
+class Dinner {
+    #dessert
+    constructor(salad, soup, entree, dessert) {
+      this.#dessert = dessert;
+      this.soup = soup;
+      this.salad = salad
+      this.entree = entree;
+    }
+}
